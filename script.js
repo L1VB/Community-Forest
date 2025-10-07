ef1f1edd3db0847bf15df51c4caabc39d533c26f
Liv B <liv.b932@gmail.com>
2025-10-07 15:17:37 +1300

    navigation change

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const hamMenu = document.querySelector('.ham-menu');
 const openMenu = document.querySelector('.menu-open');
+const openNav = document.querySelector('nav');
 
 hamMenu.addEventListener('click', () => {
     hamMenu.classList.toggle('active');
     openMenu.classList.toggle('active');
+    openNav.classList.toggle('active');
 })
