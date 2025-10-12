349dbfaf26f71e0b396ff96a4c924e240f7b5868
Liv B <liv.b932@gmail.com>
2025-10-13 12:03:52 +1300

    new pages

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
-const hamMenu = document.querySelector('.menu');
+//menu
+const menu = document.querySelector('.menu');
 const openMenu = document.querySelector('.menu-open');
 const openNav = document.querySelector('nav');
+const map = document.querySelector('.map');
 
-hamMenu.addEventListener('click', () => {
-    hamMenu.classList.toggle('active');
+menu.addEventListener('click', () => {
+    menu.classList.toggle('active');
     openMenu.classList.toggle('active');
     openNav.classList.toggle('active');
-})
+    // map.classList.toggle('disable');
+});
